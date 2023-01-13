072187132a44e58a900d5cbaef5db665848d860c
alhazmy13 <me@alhazmy13.net>
2023-01-13 06:38:36 +0300

    Add docker file, update translation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import dayjs, { Dayjs } from 'dayjs';
-import * as config from "../config.json";
+import {config} from "./config";
 import PlexApi from "./plex/api";
 import PlexHistory from "./plex/models/history";
 import Episode from "./tvtime/models/episode";
@@ -11,7 +11,7 @@ let lastCheck: Dayjs;
 let storage = new JsonStorage();
 
 (async () => {
-  print("Démarrage du bot ...");
+  print("Starting the bot ...");
   lastCheck = dayjs();
   start();
   setInterval(() => {
@@ -21,22 +21,22 @@ let storage = new JsonStorage();
 
 async function start() {
   plexHistory = await getPlexHistory();
-  print(`Il y a ${plexHistory.length} épisodes a synchroniser`);
+  print(`There are ${plexHistory.length} episodes to synchronize`);
   for (const item of plexHistory) {
     print(`${item.showTitle} - S${item.seasonNumber}E${item.episodeNumber} - ${item.episodeTitle}`);
     await findShow(item);
   }
-  print(`Prochaine vérification dans ${config.timer} minutes.`);
+  print(`Next check in ${config.timer} minutes.`);
   lastCheck = dayjs();
 }
 
 async function findShow(history: PlexHistory) {
-  print(`Recherche de la correspondance sur TVTime...`);
+  print(`Search for correspondence on TVTime...`);
   if (await storage.hasLink(history.id)) {
     let show = await TvTimeApi.getShow(await storage.getLink(history.id));
     let episode = show?.seasons[Number(history.seasonNumber) - 1].episodes[Number(history.episodeNumber) - 1];
     if (episode) {
-      await TvTimeApi.markAsWatched(episode.id) ? print(`${episode.name}: marqué comme vu.`) : print(`${episode.name}: une erreur est survenue`);
+      await TvTimeApi.markAsWatched(episode.id) ? print(`${episode.name}: marked as seen.`) : print(`${episode.name}: An error has occurred`);
     }
   } else {
     let searchs = await TvTimeApi.searchShow(history.showTitle);
@@ -51,7 +51,7 @@ async function findShow(history: PlexHistory) {
       if (show != null && episode?.airDate == history.date) {
         print(`Correspondance: ${show.name}(${show.id}) - ${episode.name}(${episode.id}) `);
         await storage.addLink(history.id, show.id);
-        await TvTimeApi.markAsWatched(episode.id) ? print(`${episode.name}: marqué comme vu.`) : print(`${episode.name}: une erreur est survenue`);
+        await TvTimeApi.markAsWatched(episode.id) ? print(`${episode.name}: marked as seen.`) : print(`${episode.name}: An error has occurred`);
       }
     }
   }
